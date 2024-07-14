ace3f2b786b7e92323f4d9df1828ab2eec976638
Ishaandeep <ishaandeep5@gmail.com>
2024-07-14 22:57:20 +0530

    change in state life cycle of  pathFindingVisualizer.jsx

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer />
+    </div>
+  );
+}
+
+export default App;
